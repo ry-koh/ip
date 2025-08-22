5c8aac20a288ba4dc549b9d572dd8920d439dc5e
Ryan <rkoh26082003@gmail.com>
2025-08-23 02:05:52 +0800

    Add greeting and exit message

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,5 +1,12 @@
 public class Hero {
+    private static final String LINE = "____________________________________________________________";
+
     public static void main(String[] args) {
-        System.out.println("Hello from Hero");
+        System.out.println(LINE);
+        System.out.println("Hello! I'm Hero");
+        System.out.println("What can I do for you?");
+        System.out.println(LINE);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
     }
 }
