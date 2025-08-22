c31d6c5623229e9f67e7f30ee1dcfb037e9ebf81
Ryan <rkoh26082003@gmail.com>
2025-08-23 02:16:27 +0800

    Add echo command to Hero

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Hero {
     private static final String LINE = "____________________________________________________________";
 
@@ -5,6 +7,21 @@ public class Hero {
         System.out.println(LINE);
         System.out.println("Hello! I'm Hero");
         System.out.println("What can I do for you?");
+        System.out.println(LINE);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                break;
+            } else {
+                System.out.println(LINE);
+                System.out.println(input);
+                System.out.println(LINE);
+            }
+        }
+        scanner.close();
         System.out.println(LINE);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(LINE);
